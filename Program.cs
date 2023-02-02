075282ac583c40522a9f74b8da678bd05d54f9c6
Wout Hebberecht <58189134+WoutHebbb@users.noreply.github.com>
2023-02-02 09:41:56 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+
+SecretClientOptions options = new SecretClientOptions()
+    {
+        Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+    };
+var client = new SecretClient(new Uri("https://secretforpipeline.vault.azure.net/"), new DefaultAzureCredential(),options);
+
+KeyVaultSecret secret = client.GetSecret("geheimpje");
+
+string secretValue = secret.Value;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => secretValue);
 
 app.Run();
